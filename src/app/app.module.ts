6327a47586c521190a1c54d75ea4ec57cf4e5e37
Adam-CCC <79598536+Adam-CCC@users.noreply.github.com>
2024-08-18 16:38:37 +0300

    delete header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
 import { MatIconModule } from '@angular/material/icon';
 import { SchemaComponent } from './schema/schema.component'
 import { HttpClientModule } from '@angular/common/http';
@@ -11,7 +10,6 @@ import { HttpClientModule } from '@angular/common/http';
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
     SchemaComponent,
   ],
   imports: [
